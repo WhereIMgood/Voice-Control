b31c4b51fc6d5d9f55ea3a9923aa7e48336df13b
WhereIMgood <120791015+WhereIMgood@users.noreply.github.com>
2023-08-16 16:04:59 +0100

    Update recorder.py
    Correcting issues

--- recorder.py
+++ recorder.py
@@ -3,27 +3,14 @@ import soundfile as sf
 import openai  # Don't forget to import the necessary library
 import threading  # Import threading for handling user input in parallel
 
-openai.api_key = "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"
+openai.api_key = "YOUR_API_KEY"
 # Set up your OpenAI API key
 
-recording = True  # A flag to indicate if recording is active
-
-def stop_recording():
-    global recording
-    input("Press Enter to stop recording...")
-    recording = False
-
 def record_audio(filename, duration, samplerate):
-    global recording
-    
     print("Listening... Press Enter to stop recording.")
     audio_data = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=1)
     
-    recording_thread = threading.Thread(target=stop_recording)
-    recording_thread.start()  # Start the thread to listen for user input
-    
     sd.wait()  # Wait for recording to finish
-    recording = False  # Ensure recording is stopped after waiting
     sf.write(filename, audio_data, samplerate)
     print("Recording stopped.")
     
